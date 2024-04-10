854793d4733dc41501901ef6efe78dd551e0f18e
Pawel <p380623@gmail.com>
2024-04-10 18:42:31 +0200

    #4

--- src/main/java/com/example/crud/dao/EmployeeDAO.java
+++ src/main/java/com/example/crud/dao/EmployeeDAO.java
@@ -1,2 +1,9 @@
-package com.example.crud.dao;public interface EmployeeDAO {
+package com.example.crud.dao;
+
+import com.example.crud.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    List<Employee> findAll();
 }
--- src/main/java/com/example/crud/rest/EmployeeRestController.java
+++ src/main/java/com/example/crud/rest/EmployeeRestController.java
@@ -1,2 +1,24 @@
-package com.example.crud.rest;public class EmployeeRestController {
+package com.example.crud.rest;
+
+import com.example.crud.dao.EmployeeDAO;
+import com.example.crud.entity.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+    private EmployeeDAO employeeDAO;
+
+    public EmployeeRestController(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findAll(){
+        return employeeDAO.findAll();
+    }
 }
